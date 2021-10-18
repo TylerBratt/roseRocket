cf481006cbce9c8645abbe22dc47551b52e81dc4
Tyler Bratt <tyler~bratt@hotmail.com>
2021-10-18 16:36:12 -0400

    fix server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const fs = require('fs')
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
